6075b620fb77ccbd5c49002664decbcfbea770e8
Raphael Braga <raffael.info@gmail.com>
2023-03-27 10:41:25 -0300

    implement photo upload in local storage

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Body, Post, Req, UseGuards } from '@nestjs/common';
+import {
+    Controller,
+    Body,
+    Post,
+    UseGuards,
+    UseInterceptors,
+    UploadedFile,
+    BadRequestException,
+    ParseFilePipe,
+    FileTypeValidator,
+    MaxFileSizeValidator,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { User } from 'src/decorators/user.decorator';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { UsersService } from 'src/users/users.service';
@@ -7,12 +19,15 @@ import { AuthForgetDto } from './dto/auth-forget.dto';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { AuthRegisterDto } from './dto/auth-register.dto';
 import { AuthResetDto } from './dto/auth-reset.dto';
+import { join } from 'path';
+import { FileService } from 'src/file/file.service';
 
 @Controller('auth')
 export class AuthController {
     constructor(
         private readonly usersService: UsersService,
         private readonly authService: AuthService,
+        private readonly fileService: FileService,
     ) {}
 
     @Post('login')
@@ -40,5 +55,38 @@ export class AuthController {
     @Post('me')
     async me(@User() user) {
         return { user };
+    }
+
+    @UseInterceptors(FileInterceptor('file'))
+    @UseGuards(AuthGuard)
+    @Post('photo')
+    async uploadPhoto(
+        @User() user,
+        @UploadedFile(
+            new ParseFilePipe({
+                validators: [
+                    new FileTypeValidator({ fileType: 'image/png' }),
+                    new MaxFileSizeValidator({ maxSize: 1024 * 50 }),
+                ],
+            }),
+        )
+        photo: Express.Multer.File,
+    ) {
+        const path = join(
+            __dirname,
+            '..',
+            '..',
+            'storage',
+            'photos',
+            `photo-${user.user_id}.png`,
+        );
+
+        try {
+            await this.fileService.upload(photo, path);
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
+
+        return { sucess: true, photo };
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersService } from 'src/users/users.service';
+import { FileModule } from 'src/file/file.module';
 
 @Module({
     imports: [
@@ -13,6 +14,7 @@ import { UsersService } from 'src/users/users.service';
         }),
         UsersModule,
         PrismaModule,
+        FileModule,
     ],
     exports: [UsersModule, PrismaModule],
     providers: [AuthService, UsersService],
